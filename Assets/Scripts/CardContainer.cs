ed465a9c4dbf864f6fef59f0c41660c018e8efb0
dnuccio80 <nuccio227@gmail.com>
2024-04-22 13:49:41 -0300

    mix cards system added
    Add the logic to mix the cards and set a random position for each card

--- Assets/Scripts/CardContainer.cs
+++ Assets/Scripts/CardContainer.cs
@@ -4,39 +4,36 @@ using UnityEngine;
 
 public class CardContainer : MonoBehaviour
 {
-
-    public List<GameObject> objectsToAlign = new List<GameObject>();
-    public Vector3 alignmentPosition = new Vector3(0, 0, 0);
-
-    // Método para agregar un nuevo objeto a la lista y alinear todos los objetos
-    public void AddAndAlignObject(GameObject newObj)
-    {
-        objectsToAlign.Add(newObj);
-        AlignAllObjects();
-    }
+    
+    List<Vector3> cardPositions = new List<Vector3>();
 
     private void Start()
     {
-        AlignAllObjects();
+        GetCardPositions();
     }
 
-
-    // Método para alinear todos los objetos en la lista
-    public void AlignAllObjects()
+    private void GetCardPositions()
     {
-        // Calcula la posición promedio de todos los objetos en la lista
-        Vector3 averagePosition = alignmentPosition;
-        foreach (GameObject obj in objectsToAlign)
+        foreach(Transform child in transform)
         {
-            averagePosition += obj.transform.position;
+            cardPositions.Add(child.localPosition);
         }
-        averagePosition /= objectsToAlign.Count;
 
-        // Alinea todos los objetos a la posición promedio
-        foreach (GameObject obj in objectsToAlign)
-        {
-            obj.transform.position = averagePosition;
-        }
+        MixCards();
     }
+
+    private void MixCards()
+    {
+        foreach (Transform child in transform)
+        {
+            int cardIndex = Random.Range(0, cardPositions.Count);
+            child.localPosition = cardPositions[cardIndex];
+            cardPositions.RemoveAt(cardIndex);
+        }
+
+
+    }
+
+
 
 }
