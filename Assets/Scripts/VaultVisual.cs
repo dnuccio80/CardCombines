b21c674557831e7034d3b88e7544d8bc675161a8
dnuccio80 <nuccio227@gmail.com>
2024-05-03 14:18:06 -0300

    Fusion vault Visual
    fusion of CoinVisual, extraTimeVisual, showMatchcardVisual & showAllcardVisual, in an unique script: VaultVisual, to update visual on shop

---
+++ Assets/Scripts/VaultVisual.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class VaultVisual : MonoBehaviour
+{
+
+    [SerializeField] private TextMeshProUGUI coinAmountText;
+    [SerializeField] private TextMeshProUGUI extraTimeAmountText;
+    [SerializeField] private TextMeshProUGUI showMatchCardAmountText;
+    [SerializeField] private TextMeshProUGUI showAllCardsAmountTexts;
+
+    private void Start()
+    {
+        PlayerStats.OnCoinsAmountChange += PlayerStats_OnCoinsAmountChange;
+        PlayerStats.OnShowCardMatchAmountChange += PlayerStats_OnShowCardMatchAmountChange;
+        PlayerStats.OnExtraTimeAmountChange += PlayerStats_OnExtraTimeAmountChange;
+        PlayerStats.OnShowAllCardsAmontChange += PlayerStats_OnShowAllCardsAmontChange;
+    }
+
+    private void PlayerStats_OnShowAllCardsAmontChange(object sender, System.EventArgs e)
+    {
+        UpdateVisual();
+
+    }
+
+    private void PlayerStats_OnExtraTimeAmountChange(object sender, System.EventArgs e)
+    {
+        UpdateVisual();
+
+    }
+
+    private void PlayerStats_OnCoinsAmountChange(object sender, System.EventArgs e)
+    {
+        UpdateVisual();
+
+    }
+
+    private void PlayerStats_OnShowCardMatchAmountChange(object sender, System.EventArgs e)
+    {
+        UpdateVisual();
+
+    }
+
+
+    private void UpdateVisual()
+    {
+        coinAmountText.text = "x" + PlayerStats.GetCoinsAmount();
+        extraTimeAmountText.text = "x" + PlayerStats.GetExtraTimePotionAmount();
+        showMatchCardAmountText.text = "x" + PlayerStats.GetShowMatchCardPotionAmount();
+        showAllCardsAmountTexts.text = "x" + PlayerStats.GetShowAllCardsPotionAmount();
+    }
+
+}
