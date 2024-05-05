49a27321e5fda9b1f91021010cf0fd7e79471f56
dnuccio80 <nuccio227@gmail.com>
2024-05-04 21:50:25 -0300

    new subcription event on SellMenu
    added a subcription on ShopUI-OnDealButtonPressed, to update quantity visual on sellMenu

--- Assets/Scripts/SellPotionsMenu.cs
+++ Assets/Scripts/SellPotionsMenu.cs
@@ -12,10 +12,23 @@ public class SellPotionsMenu : MonoBehaviour
     [SerializeField] private TextMeshProUGUI ShowMatchCardAmountText;
     [SerializeField] private TextMeshProUGUI ShowAllCardAmountText;
 
+    private ShopUI shopUI;
+
+    private void Awake()
+    {
+        shopUI = GetComponentInParent<ShopUI>();
+    }
+
     private void Start()
     {
         UpdateVisual();
+        shopUI.OnDealButtonPressed += ShopUI_OnDealButtonPressed;
         Hide();
+    }
+
+    private void ShopUI_OnDealButtonPressed(object sender, EventArgs e)
+    {
+        UpdateVisual();
     }
 
     private void OnEnable()
