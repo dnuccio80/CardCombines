4d25ffbb79e8fb5de3532e09e150a41bd5851632
dnuccio80 <nuccio227@gmail.com>
2024-05-16 13:54:52 -0300

    Game Finished

--- Assets/Scripts/HowToPlayMenu.cs
+++ Assets/Scripts/HowToPlayMenu.cs
@@ -5,26 +5,35 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class HowToPlayMenu : MonoBehaviour
-{
+{ 
 
     [SerializeField] private Transform[] demoTransformArray;
-    [SerializeField] private Button[] nextButtonArray;
-    [SerializeField] private Button[] backButtonArray;
+
+    [Header("Next Buttons")]
+    [SerializeField] private Button demo1_NextButton;
+    [SerializeField] private Button demo2_NextButton;
+    [SerializeField] private Button demo3_NextButton;
+    [SerializeField] private Button demo4_NextButton;
+    [SerializeField] private Button demo5_NextButton;
+    [SerializeField] private Button demo6_NextButton;
+    [SerializeField] private Button demo7_NextButton;
+    [SerializeField] private Button demo8_NextButton;
+
+    [Header("Back Buttons")]
+    [SerializeField] private Button demo1_BackButton;
+    [SerializeField] private Button demo2_BackButton;
+    [SerializeField] private Button demo3_BackButton;
+    [SerializeField] private Button demo4_BackButton;
+    [SerializeField] private Button demo5_BackButton;
+    [SerializeField] private Button demo6_BackButton;
+    [SerializeField] private Button demo7_BackButton;
+    [SerializeField] private Button demo8_BackButton;
+
+
 
     private void Awake()
     {
-        for (int i = 0; i < nextButtonArray.Length; i++)
-        {
-            //if (i == nextButtonArray.Length -1) continue;
-
-            nextButtonArray[i].onClick.AddListener(() =>
-            {
-                Debug.Log("Tocando "  + demoTransformArray[i]);
-                //demoTransformArray[i].gameObject.SetActive(false);
-                //demoTransformArray[i+1].gameObject.SetActive(true);
-            });
-        }
-
+        InitButtons();
 
     }
 
@@ -39,15 +48,129 @@ public class HowToPlayMenu : MonoBehaviour
         Show();
     }
 
+    private void InitButtons()
+    {
+        // Next Buttons
+        demo1_NextButton.onClick.AddListener(() =>
+        {
+            demoTransformArray[0].gameObject.SetActive(false);
+            demoTransformArray[1].gameObject.SetActive(true);
+            demo2_NextButton.Select();
+        });
+
+        demo2_NextButton.onClick.AddListener(() =>
+        {
+            demoTransformArray[1].gameObject.SetActive(false);
+            demoTransformArray[2].gameObject.SetActive(true);
+            demo3_NextButton.Select();
+        });
+
+        demo3_NextButton.onClick.AddListener(() =>
+        {
+            demoTransformArray[2].gameObject.SetActive(false);
+            demoTransformArray[3].gameObject.SetActive(true);
+            demo4_NextButton.Select();
+        });
+
+        demo4_NextButton.onClick.AddListener(() =>
+        {
+            demoTransformArray[3].gameObject.SetActive(false);
+            demoTransformArray[4].gameObject.SetActive(true);
+            demo5_NextButton.Select();
+        });
+
+        demo5_NextButton.onClick.AddListener(() =>
+        {
+            demoTransformArray[4].gameObject.SetActive(false);
+            demoTransformArray[5].gameObject.SetActive(true);
+            demo6_NextButton.Select();
+        });
+
+
+        demo6_NextButton.onClick.AddListener(() =>
+        {
+            demoTransformArray[5].gameObject.SetActive(false);
+            demoTransformArray[6].gameObject.SetActive(true);
+            demo7_NextButton.Select();
+        });
+
+        demo7_NextButton.onClick.AddListener(() =>
+        {
+            demoTransformArray[6].gameObject.SetActive(false);
+            demoTransformArray[7].gameObject.SetActive(true);
+            demo8_NextButton.Select();
+        });
+
+        demo8_NextButton.onClick.AddListener(() =>
+        {
+            Hide();
+        });
+
+
+        //Back Buttons
+        demo1_BackButton.onClick.AddListener(() =>
+        {
+            Hide();
+        });
+
+        demo2_BackButton.onClick.AddListener(() =>
+        {
+            demoTransformArray[0].gameObject.SetActive(true);
+            demoTransformArray[1].gameObject.SetActive(false);
+            demo1_NextButton.Select();
+        });
+
+        demo3_BackButton.onClick.AddListener(() =>
+        {
+            demoTransformArray[1].gameObject.SetActive(true);
+            demoTransformArray[2].gameObject.SetActive(false);
+            demo2_NextButton.Select();
+        });
+
+        demo4_BackButton.onClick.AddListener(() =>
+        {
+            demoTransformArray[2].gameObject.SetActive(true);
+            demoTransformArray[3].gameObject.SetActive(false);
+            demo3_NextButton.Select();
+        });
+
+        demo5_BackButton.onClick.AddListener(() =>
+        {
+            demoTransformArray[3].gameObject.SetActive(true);
+            demoTransformArray[4].gameObject.SetActive(false);
+            demo4_NextButton.Select();
+        });
+
+        demo6_BackButton.onClick.AddListener(() =>
+        {
+            demoTransformArray[4].gameObject.SetActive(true);
+            demoTransformArray[5].gameObject.SetActive(false);
+            demo5_NextButton.Select();
+        });
+
+        demo7_BackButton.onClick.AddListener(() =>
+        {
+            demoTransformArray[5].gameObject.SetActive(true);
+            demoTransformArray[6].gameObject.SetActive(false);
+            demo6_NextButton.Select();
+        });
+
+        demo8_BackButton.onClick.AddListener(() =>
+        {
+            demoTransformArray[6].gameObject.SetActive(true);
+            demoTransformArray[7].gameObject.SetActive(false);
+            demo7_NextButton.Select();
+        });
+    }
+
     private void Show()
     {
         gameObject.SetActive(true);
 
-        for(int i = 0; i < demoTransformArray.Length; i++)
+        for (int i = 0; i < demoTransformArray.Length; i++)
         {
-            if (i == 0) continue;
-
-            demoTransformArray[i].gameObject.SetActive(false);
+            if (i == 0) demoTransformArray[i].gameObject.SetActive(true);
+            else demoTransformArray[i].gameObject.SetActive(false);
         }
     }
 
